1de418653ff1aca137aee10cf9f54f77ec6b664f
RamSSi <001229aram@gmail.com>
2022-05-24 05:21:37 +0900

    error...

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
+
+
 const API_KEY = "962f8b6b6cfad0728c32939691af7250";
-let lat = 37;
-let lon = 127;
+let lat;
+let lon;
 let city;
 let weatherImg;
 
 const temp = document.querySelector("#temp");
 const icon = document.querySelector("img");
 
+icon.style.display = "none";
 document.querySelector(".btn-group").addEventListener("click", (e) => {
   if (e.target.getAttribute("type") === "button")
     city = e.target.getAttribute("id");
   else return;
 
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      lat = data[0].lat;
-      lon = data[0].lon;
+  axios(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`)
+    // .then(response => response.json())
+    .then(response => {
+      lat = response.data[0].lat;
+      lon = response.data[0].lon;
     })
     .then(() => {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-          
-          weatherImg = data.weather[0].icon;
-          temp.innerText = `온도 : ${Math.round(data.main.temp - 273.15)}`;
+      axios(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+        // .then(response => response.json())
+        .then(response => {
+          weatherImg = response.data.weather[0].icon;
+          temp.innerText = `온도 : ${Math.round(response.data.main.temp - 273.15)}`;
+          icon.style.display = "block";
           icon.src = `http://openweathermap.org/img/wn/${weatherImg}@2x.png`;
         });
     })
